557574ed0447c334b47ea9c2e4ca54dc8d3518ed
iliazark <56551628+iliazark@users.noreply.github.com>
2024-10-16 17:11:51 +0300

    Update app/FreeRTOSTasks/Src/eMMCTask.cpp
    Co-authored-by: Andronikos <58566096+AndronikosKostas@users.noreply.github.com>

--- app/FreeRTOSTasks/Src/eMMCTask.cpp
+++ app/FreeRTOSTasks/Src/eMMCTask.cpp
@@ -57,7 +57,7 @@ void eMMCTask::execute() {
         LOG_DEBUG << "Writing to block address: " << block_address_a;
         status = eMMC::writeBlockEMMC(data_buff, block_address_a,1);
         if(status.has_value()){
-            //read was successful
+            // write was successful
 
         }else if(status.error() != eMMC::Error::NO_ERRORS){
             //handle the errors
