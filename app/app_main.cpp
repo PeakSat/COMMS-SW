a770c036c06700cd836b494046e484738da483dd
maestroque <kikasgeorg@ece.auth.gr>
2024-10-21 16:34:50 +0300

    infra: Add clang-format pre-commit hook

--- app/app_main.cpp
+++ app/app_main.cpp
@@ -8,27 +8,25 @@
 #include "UARTGatekeeperTask.hpp"
 #include "eMMCTask.hpp"
 
+void app_main(void) {
+	transceiverTask.emplace();
+	uartGatekeeperTask.emplace();
+	eMMCTask.emplace();
 
-void app_main( void )
-{
-    transceiverTask.emplace();
-    uartGatekeeperTask.emplace();
-    eMMCTask.emplace();
+	transceiverTask->createTask();
+	uartGatekeeperTask->createTask();
+	eMMCTask->createTask();
 
-    transceiverTask->createTask();
-    uartGatekeeperTask->createTask();
-    eMMCTask->createTask();
+	/* Start the scheduler. */
+	vTaskStartScheduler();
 
-    /* Start the scheduler. */
-    vTaskStartScheduler();
-
-    /* Should not get here. */
-    for(;;);
+	/* Should not get here. */
+	for (;;)
+		;
 }
 /*-----------------------------------------------------------*/
 
-
 extern "C" [[maybe_unused]] void EXTI1_IRQHandler(void) {
-    HAL_GPIO_EXTI_IRQHandler(RF_IRQ_Pin);
-    transceiverTask->transceiver.handle_irq();
+	HAL_GPIO_EXTI_IRQHandler(RF_IRQ_Pin);
+	transceiverTask->transceiver.handle_irq();
 }
