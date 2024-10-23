c2c1aa6dbea9bdf51c4cf12f85f20816554af6e9
maestroque <kikasgeorg@ece.auth.gr>
2024-10-23 11:27:55 +0300

    infra: Better pre-commit exclude regex

--- Drivers/eMMC/Inc/eMMC.hpp
+++ Drivers/eMMC/Inc/eMMC.hpp
@@ -1,43 +1,42 @@
 #pragma once
 
-#include "stm32h7xx_hal_mmc.h"
 #include "etl/expected.h"
+#include "stm32h7xx_hal_mmc.h"
 
 
+namespace eMMC {
+	// Error status
+	enum class Error : uint8_t {
+		NO_ERRORS = 0,
+		EMMC_READ_FAILURE,
+		EMMC_WRITE_FAILURE,
+		EMMC_ERASE_BLOCK_FAILURE,
+	};
 
-namespace eMMC{
-    // Error status
-    enum class Error : uint8_t {
-        NO_ERRORS = 0,
-        EMMC_READ_FAILURE,
-        EMMC_WRITE_FAILURE,
-        EMMC_ERASE_BLOCK_FAILURE,
-    };
-
-    /**
+	/**
      * 
      * @param write_data 
      * @param block_address 
      * @param NumberOfBlocks 
      * @return 
      */
-    etl::expected<void, Error>  writeBlockEMMC(uint8_t* write_data, uint32_t block_address, uint32_t NumberOfBlocks);
+	etl::expected<void, Error> writeBlockEMMC(uint8_t* write_data, uint32_t block_address, uint32_t NumberOfBlocks);
 
-    /**
+	/**
      * 
      * @param read_data 
      * @param block_address 
      * @param NumberOfBlocks 
      * @return 
      */
-    etl::expected<void, Error> readBlockEMMC(uint8_t* read_data, uint32_t block_address, uint32_t NumberOfBlocks);
+	etl::expected<void, Error> readBlockEMMC(uint8_t* read_data, uint32_t block_address, uint32_t NumberOfBlocks);
 
-    /**
+	/**
      * 
      * @param block_address_start 
      * @param block_address_end 
      * @return 
      */
-    etl::expected<void, Error>  eraseBlocksEMMC(uint32_t block_address_start, uint32_t block_address_end);
-    
-}
+	etl::expected<void, Error> eraseBlocksEMMC(uint32_t block_address_start, uint32_t block_address_end);
+
+} // namespace eMMC
--- app/FreeRTOSTasks/Inc/eMMCTask.hpp
+++ app/FreeRTOSTasks/Inc/eMMCTask.hpp
@@ -7,21 +7,21 @@
 
 class eMMCTask : public Task {
 private:
-    const static inline uint16_t DelayMs = 60000;
-    const static inline uint16_t TaskStackDepth = 2000;
+	const static inline uint16_t DelayMs = 60000;
+	const static inline uint16_t TaskStackDepth = 2000;
 
-    StackType_t taskStack[TaskStackDepth];
+	StackType_t taskStack[TaskStackDepth];
+
 public:
+	void execute();
 
-    void execute();
+	eMMCTask() : Task("eMMC Memory Functions") {}
 
-    eMMCTask() : Task("eMMC Memory Functions") {}
-
-    void createTask(){
-        xTaskCreateStatic(vClassTask<eMMCTask>, this->TaskName,
-                          eMMCTask::TaskStackDepth, this, tskIDLE_PRIORITY + 1,
-                          this->taskStack, &(this->taskBuffer));
-    }
+	void createTask() {
+		xTaskCreateStatic(vClassTask<eMMCTask>, this->TaskName,
+		                  eMMCTask::TaskStackDepth, this, tskIDLE_PRIORITY + 1,
+		                  this->taskStack, &(this->taskBuffer));
+	}
 };
 
 inline etl::optional<eMMCTask> eMMCTask;
