c365aefeec54241e8fe7916e0068090ed696cadd
Ilia Zarka <iliazark@gamil.com>
2024-10-23 18:38:23 +0300

    test: added memory map

--- app/app_main.cpp
+++ app/app_main.cpp
@@ -10,7 +10,49 @@
 
 struct eMMCTransactionHandler eMMCTransactionHandler;
 
+uint32_t memoryCapacity = 0xE90E80;
+uint32_t memoryPageSize = 512; // bytes
+
+enum memoryItem {
+    firmware,
+    GNSSData,
+    memoryItemCount
+};
+
+struct memoryItemData {
+    memoryItemData() : size(0), startAddress(), endAddress() {}
+    explicit memoryItemData(uint32_t newSize)
+        : size(newSize), startAddress(0), endAddress(0) {}
+    uint32_t size;         // Item size in bytes
+    uint32_t startAddress; // Calculated automatically on boot
+    uint32_t endAddress;   // Calculated automatically on boot
+};
+
 void app_main(void) {
+
+    std::array<memoryItemData, memoryItemCount> memoryMap;
+
+    memoryMap[firmware] = memoryItemData(1000000);
+    memoryMap[GNSSData] = memoryItemData(3000000);
+
+    uint32_t headPointer = 0;
+    for (int i = 0; i < memoryItemCount; i++) {
+        headPointer += memoryMap[i].size;
+        if (headPointer > memoryCapacity) {
+            // memory full
+        } else {
+            memoryMap[i].endAddress = headPointer;
+            memoryMap[i].startAddress = headPointer - memoryMap[i].size;
+        }
+
+        // allign for page size (512 bytes)
+        headPointer += memoryPageSize - (headPointer % memoryPageSize);
+    }
+
+    if (memoryMap[0].endAddress != 0) {
+        __NOP();
+    }
+
     eMMCTransactionHandler.eMMC_semaphore = xSemaphoreCreateMutex();
 
     transceiverTask.emplace();
