910ccd47420bcda83d15cb56d0dc67980561697b
andronikos <andronikostaslos@gmail.com>
2025-01-15 19:48:43 +0200

    test: RF Conops close to victory
    Description: Decreased the delay for the ensure RX function. Also observed a state invalid which does now cause issues...maybe it will cause so the code needs to control even better the AT and its states.

--- app/FreeRTOSTasks/Src/RF_RXTask.cpp
+++ app/FreeRTOSTasks/Src/RF_RXTask.cpp
@@ -5,7 +5,6 @@
 using namespace AT86RF215;
 
 void RF_RXTask::ensureRxMode() {
-
     if (transceiver.get_state(RF09, error) != State::RF_RX) {
         transceiver.set_state(RF09, RF_TRXOFF, error);
         vTaskDelay(10);
@@ -18,15 +17,12 @@ void RF_RXTask::ensureRxMode() {
 }
 void RF_RXTask::execute() {
     vTaskDelay(5000);
-    LOG_INFO << "RF RX TASK";
+    LOG_INFO << "[RF RX TASK]";
     /// Set the Up-link frequency
     HAL_GPIO_WritePin(P5V_RF_EN_GPIO_Port, P5V_RF_EN_Pin, GPIO_PIN_SET);
-    LOG_INFO << "5V CHANNEL ON";
-    // ENABLE THE RX SWITCH
+    /// ENABLE THE RX SWITCH
     HAL_GPIO_WritePin(EN_RX_UHF_GPIO_Port, EN_RX_UHF_Pin, GPIO_PIN_RESET);
-    LOG_DEBUG << "RX SWITCH ENABLED ";
     HAL_GPIO_WritePin(EN_UHF_AMP_RX_GPIO_Port, EN_UHF_AMP_RX_Pin, GPIO_PIN_RESET);
-    LOG_DEBUG << "RX AMP ENABLED ";
     /// Essential for the trx to be able to send and receive packets
     /// (If you have it HIGH from the CubeMX the trx will not be able to send)
     HAL_GPIO_WritePin(RF_RST_GPIO_Port, RF_RST_Pin, GPIO_PIN_RESET);
@@ -37,27 +33,31 @@ void RF_RXTask::execute() {
     if (xSemaphoreTake(TransceiverHandler::transceiver_semaphore, portMAX_DELAY) == pdTRUE) {
         auto status = transceiver.check_transceiver_connection(error);
         if (status.has_value()) {
-            LOG_INFO << "AT86RF215 CONNECTION OK";
         } else
-            /// TODO: Error handling
-            LOG_ERROR << "AT86RF215 ##ERROR## WITH CODE: " << status.error();
+            LOG_ERROR << "CONNECTION ##ERROR## WITH CODE: " << status.error();
         transceiver.configure_pll(AT86RF215::RF09, transceiver.freqSynthesizerConfig.channelCenterFrequency09, transceiver.freqSynthesizerConfig.channelNumber09, transceiver.freqSynthesizerConfig.channelMode09, transceiver.freqSynthesizerConfig.loopBandwidth09, transceiver.freqSynthesizerConfig.channelSpacing09, error);
         transceiver.setup(error);
         xSemaphoreGive(TransceiverHandler::transceiver_semaphore);
     }
-    uint16_t received_length;
     transceiver.set_state(AT86RF215::RF09, State::RF_TXPREP, error);
     vTaskDelay(pdMS_TO_TICKS(20));
     transceiver.set_state(AT86RF215::RF09, State::RF_RX, error);
     if (transceiver.get_state(RF09, error) == RF_RX)
-        LOG_INFO << "[RX TASK] INITIAL STATE = RX";
-    uint32_t receivedEvents = 0;
+        LOG_INFO << "[RF RX TASK] INITIAL STATE = RX";
     HAL_GPIO_WritePin(EN_UHF_AMP_RX_GPIO_Port, EN_UHF_AMP_RX_Pin, GPIO_PIN_SET);
-    LOG_DEBUG << "RX AMP ENABLED ";
-    TickType_t lastTxNotifyTime = xTaskGetTickCount(); // Reference time for TX notification
-//    xTaskNotify(rf_txtask->taskHandle, START_TX_TASK, eSetBits);
+    /// Reference time for TX notification
+    TickType_t lastTxNotifyTime = xTaskGetTickCount();
+    xTaskNotify(rf_txtask->taskHandle, START_TX_TASK, eSetBits);
+    uint32_t receivedEvents = 0;
+    uint16_t received_length;
     while (1) {
-        if (xTaskNotifyWait(0, 0xFFFFFFFF, &receivedEvents, pdMS_TO_TICKS(50))) {
+        /// Notify TX task
+        /// If you send TM with a period smaller than 2s you may lose packets on receive. But we are okay with that because we send housekeeping every 1 min
+        if ((xTaskGetTickCount() - lastTxNotifyTime) >= pdMS_TO_TICKS(5000)) {
+            xTaskNotify(rf_txtask->taskHandle, TRANSMIT, eSetBits);
+            lastTxNotifyTime = xTaskGetTickCount();
+        }
+        if (xTaskNotifyWait(0, 0xFFFFFFFF, &receivedEvents, pdMS_TO_TICKS(20))) {
             if (receivedEvents & RXFE) {
                 if (xSemaphoreTake(TransceiverHandler::transceiver_semaphore, portMAX_DELAY) == pdTRUE) {
                     auto result = transceiver.get_received_length(RF09, error);
@@ -80,12 +80,5 @@ void RF_RXTask::execute() {
             }
         }
         ensureRxMode();
-        // Notify TX task every 5 seconds
-        if ((xTaskGetTickCount() - lastTxNotifyTime) >= pdMS_TO_TICKS(5000)) {
-            xTaskNotify(rf_txtask->taskHandle, TRANSMIT, eSetBits); // Notify TX task
-            lastTxNotifyTime = xTaskGetTickCount(); // Reset reference time
-            LOG_INFO << "Notified TX task to start transmission.";
-        }
-
     }
 }
